3a5c9cece46d8edd8385c797a5473cc67b2ecfb0
Omochice-E130 <Omochice.MgrNnm@vivaldi.net>
2020-01-10 01:37:08 +0900

    サンプルデータで動くようにした

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, flash, g, redirect, render_template, request, url_for, send_from_directory
 import models
 import os
+import json
 
 app = Flask(__name__)
+
+with open("category.json", "r") as f:
+    categories = [{"en": item[0], "jp": item[1]} for item in json.load(f)]
 
 
 @app.route('/')
 def top_page():
     # top page
-    return render_template('top_page.html')
+    return render_template('top_page.html', categories=categories)
 
 
 @app.route('/favicon.ico')
@@ -21,29 +25,39 @@ def favicon():
 @app.route("/about")
 def about():
     # about page
-    return render_template("about.html")
+    return render_template("about.html", categories=categories)
 
 
 @app.route("/items/<category>")
 def view_catogory(category):
-    items = models.get_category(category)
-    return render_template("list.html", items=items)
+    results = models.get_category(category)
+    items = [{
+        "item_id": f"{result[1]}-{result[0]}",
+        "img_path": f"img/{result[1]}-{result[0]}.jpg",
+        "key": result[0]
+    } for result in results]
+    return render_template("list.html", items=items, categories=categories)
 
 
 @app.route("/items/view/<key>")
 def view_item(key):
     item = models.get_item(key)
+    print(item)
     result = {
         "item_id": f"{item[1]}-{item[0]}",
-        "img_path": f"{item[1]}-{item[0]}.jpg",
-        "category": models.translate_catogory_name(item[1]),
+        "img_path": f"img/{item[1]}-{item[0]}.jpg",
+        "category": f"{item[1]}: {models.translate_catogory_name(item[1])}",
+        "model_number": item[2],
+        "price": item[3],
+        "guide": item[4],
+        "remarks": item[5]
     }
-    return render_template("view.html")
+    return render_template("view.html", item=result, categories=categories)
 
 
 @app.route("/items/view/test")
 def view_test():
-    return render_template("view.html", img_name="test.png")
+    return render_template("view_test.html", img_name="test.png", categories=categories)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,17 +1,31 @@
 import pandas as pd
+import json
 
 filename = "test.csv"
 
 
+def load_filled_csv(csv_name: str):
+    df = pd.read_csv(csv_name)
+    return df.fillna("")
+
+
 def get_category(key: str) -> list:
-    df = pd.read_csv(filename)
-    return df.query(f"category == '{key}'").values.tolist()
+    df = load_filled_csv(filename)
+    return df.query(f"カテゴリ == '{key}'").values.tolist()
 
 
 def get_item(key: str) -> list:
-    df = pd.read_csv(filename)
-    return df.query(f"id == '{key}'").values.tolist()
+    df = load_filled_csv(filename)
+    return df.query("ID == @key").values.tolist()[0]
+
+
+def translate_catogory_name(category_name: str) -> str:
+    with open("category.json", "r") as f:
+        d = json.load(f)
+    d = {item[0]: item[1] for item in d}
+    return d[category_name]
 
 
 if __name__ == "__main__":
     print(get_category("A"))
+    print(get_item("4"))
