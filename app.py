d050ed8d019c9570829e7ffef46af2e5c2d0fc39
Omochice-E130 <Omochice.MgrNnm@vivaldi.net>
2020-01-09 22:36:12 +0900

    fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, flash, g, redirect, render_template, request, url_for
+from flask import Flask, flash, g, redirect, render_template, request, url_for, send_from_directory
 import models
+import os
 
 app = Flask(__name__)
 
@@ -8,6 +9,13 @@ app = Flask(__name__)
 def top_page():
     # top page
     return render_template('top_page.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='img/favicon.ico')
 
 
 @app.route("/about")
@@ -25,7 +33,17 @@ def view_catogory(category):
 @app.route("/items/view/<key>")
 def view_item(key):
     item = models.get_item(key)
+    result = {
+        "item_id": f"{item[1]}-{item[0]}",
+        "img_path": f"{item[1]}-{item[0]}.jpg",
+        "category": models.translate_catogory_name(item[1]),
+    }
     return render_template("view.html")
+
+
+@app.route("/items/view/test")
+def view_test():
+    return render_template("view.html", img_name="test.png")
 
 
 if __name__ == '__main__':
